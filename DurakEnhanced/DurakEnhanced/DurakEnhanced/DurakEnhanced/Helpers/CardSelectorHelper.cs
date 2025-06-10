88418dc48ba46d16d4b829a74fff12bcda84a2a8
Bivol Maksym <maksym.bivol@student.nhlstenden.com>
2025-06-10 14:20:00 +0200

    Merge frontend files from front

---
+++ DurakEnhanced/DurakEnhanced/DurakEnhanced/DurakEnhanced/Helpers/CardSelectorHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DurakEnhanced;
+using System.Windows.Forms;
+
+namespace DurakEnhanced.Helpers
+{
+    public static class CardSelectorHelper
+    {
+        public static void HandleCardClick(ref Button selectedCard, Button clickedCard, int liftOffset)
+        {
+            if (selectedCard != null && selectedCard != clickedCard)
+                selectedCard.Location = new Point(selectedCard.Location.X, selectedCard.Location.Y + liftOffset);
+
+            if (clickedCard != selectedCard)
+            {
+                clickedCard.Location = new Point(clickedCard.Location.X, clickedCard.Location.Y - liftOffset);
+                selectedCard = clickedCard;
+            }
+            else
+            {
+                clickedCard.Location = new Point(clickedCard.Location.X, clickedCard.Location.Y + liftOffset);
+                selectedCard = null;
+            }
+        }
+    }
+}
+

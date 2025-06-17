23425d447e8513e85dca9f437dc52ddca8323ac8
BramVeninga <bram.veninga@student.nhlstenden.com>
2025-06-17 11:39:40 +0200

    added basic game engine

---
+++ DurakEnhanced/DurakEnhanced/DurakEnhanced/DurakEnhanced/gameLogic/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakTCPTest.gameLogic
+{
+    public enum Suit { Hearts, Diamonds, Clubs, Spades }
+    public enum Rank { Six = 6, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
+
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+
+        public override string ToString() => $"{Rank} of {Suit}";
+    }
+
+}
---
+++ DurakEnhanced/DurakEnhanced/DurakEnhanced/DurakEnhanced/gameLogic/GameState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakTCPTest.gameLogic
+{
+    public class GameState
+    {
+        public List<Card> Deck { get; set; }
+        public List<(Card Attack, Card? Defense)> CurrentRound { get; set; }
+
+        public PlayerState Host { get; set; }
+        public PlayerState Guest { get; set; }
+
+        public string TrumpSuit { get; set; }
+        public string CurrentAttackerName { get; set; }
+        public string CurrentDefenderName { get; set; }
+
+        public string Phase { get; set; } // bv. "Waiting", "Attacking", "Defending", "RoundEnded"
+    }
+
+    public class PlayerState
+    {
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public List<Card> Hand { get; set; } // Optioneel voor jezelf zichtbaar
+        public List<ActionCard> ActionCards { get; set; }
+    }
+
+}
